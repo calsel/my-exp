bf5594cc971bb0dd9798a9fbae43e1713cf9f7d3
calsel <144112317+calsel@users.noreply.github.com>
2025-11-17 19:01:48 +0500

    Add files via upload

---
+++ angular1/src/main.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {CarsComponent} from "./app/cars/cars.component";
+
+// @ts-ignore
+@Component({
+    selector: 'app-root',
+    imports: [CarsComponent],
+    template: `<app-cars></app-cars>`,
+})
+export class App {
+    name = 'Angular';
+}
+
+bootstrapApplication(App);
